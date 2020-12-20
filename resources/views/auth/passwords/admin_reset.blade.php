e5717b297e5dc2a2a7c84c72178c876ea9dfd8d2
zabiRahimi <rahimi.z1360@gmail.com>
2020-12-20 10:29:01 +0330

    30 azar 99

--- resources/views/auth/passwords/admin_reset.blade.php
+++ resources/views/auth/passwords/admin_reset.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('admin.password.update') }}">
                         @csrf
-
+                        
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
